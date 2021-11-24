29a88c40654d70fb2422be9cd03c3afe1375772f
Elias <elias@floreteng.se>
2021-11-24 23:51:21 +0100

    idk

--- main.pl
+++ main.pl
@@ -6,30 +6,52 @@ nth1_2d(Row, Column, List, Element) :-
     nth1(Column, SubList, Element).
 
 read_file(Filename, Symbol) :-
-    see(Filename),     % Loads the input-file
-    read(Symbol),            % Reads the first Prolog-term from the file
+    see(Filename),          % Loads the input-file
+    read(Symbol),           % Reads the first Prolog-term from the file
     seen.                   % Closes the io-stream'
 
-
 % Top-level predicate
-alive(Row, Column, BoardFileName):-
-    see(BoardFileName),     % Loads the input-file
-    read(Board),            % Reads the first Prolog-term from the file
-    seen,                   % Closes the io-stream
-    check_alive(Row, Column, Board).
+% alive(Row, Column, BoardFileName) :-
+%     read_file(BoardFileName, Board).
+%     % check_ive(Row, Column, Board).
 
 % Checks whether the group of stones connected to
 % the stone located at (Row, Column) is alive or dead.
-check_alive(Row, Column, Board):-
+% check_alive(Row, Column, Board) :-
+%     \+ is_occupied(Row, Column, Board), !;
+%     neighbour(Row, Column, Row_, Column_),
+%     same_color(Row, Column, Row_, Column_, Board),
+%     check_alive(Row_, Column_, Board).
+
+
+same_color(Row, Col, Row_, Col_, Board) :-
+    nth1_2d(Row, Col, Board, Piece),
+    nth1_2d(Row_, Col_, Board, Piece_),
+    Piece = Piece_.
+
+% cell_alive(Row, Col, Board) :-
+%     neighbours(Row, Col, Board, Cell),
+%     Cell = e, !.
+
+is_occupied(Row, Column, Board) :-
     nth1_2d(Row, Column, Board, Stone),
     (Stone = b; Stone = w).
 
-neighbour(Row, Col, Row1, Col1) :-
-    Col = Col1,
+% neighbour(Row, Col, Row1, Col1, B) :-
+%     neighbour(Row, Col, Row_, Col_, B),
+%     neighbour(Row_, Col_, Row1, Col1, B).
+neighbour(Row, Col, Row1, Col1, Board) :-
+    (Col = Col1,
     (Row1 is Row + 1; Row1 is Row - 1);
     Row = Row1,
-    (Col1 is Col + 1; Col1 is Col - 1).
+    (Col1 is Col + 1; Col1 is Col - 1)),
+    same_color(Row, Col, Row1, Col1, Board).
 
-neighbours(Board, Row, Col, Neighbour) :-
-    neighbour(Row, Col, Row1, Col1),
-    nth1_2d(Row1, Col1, Board, Neighbour).
+% Check if group is alive
+check_alive(Row, Column, Board, Visited) :-
+    \+ member((Row, Column), Visited),
+    is_occupied(Row, Column, Board).
+
+% neighbours(Row, Col, Row_, Col_, Board, Neighbour) :-
+%     neighbour(Row, Col, Row1, Col1, Board),
+%     nth1_2d(Row1, Col1, Board, Neighbour).
