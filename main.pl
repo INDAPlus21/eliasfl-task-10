0da3a4bae38e93baae7f053ee817214c112efca8
Elias <elias@floreteng.se>
2021-11-26 14:29:58 +0100

    Removed unused

--- main.pl
+++ main.pl
@@ -18,27 +18,6 @@ alive(Row, Column, BoardFileName) :-
     (Stone = b; Stone = w),
     once(check_alive(Row, Column, Board, Stone, [])).
 
-same_color(Row, Col, Row_, Col_, Board) :-
-    nth1_2d(Row, Col, Board, Piece),
-    nth1_2d(Row_, Col_, Board, Piece_),
-    Piece = Piece_.
-
-is_empty(Row, Column, Board) :-
-    nth1_2d(Row, Column, Board, Stone),
-    \+ (Stone = b; Stone = w).
-
-% Assign Row_, Col_ to neighbours of Row, Col
-neighbour(Row, Col, Row_, Col_) :-
-    (Col = Col_,
-    (Row_ is Row + 1; Row_ is Row - 1);
-    Row = Row_,
-    (Col_ is Col + 1; Col_ is Col - 1)).
-
-% Neighbours with Color
-neighbours(Row, Col, Row_, Col_, Board, Color) :-
-    neighbour(Row, Col, Row_, Col_),
-    nth1_2d(Row_, Col_, Board, Color).
-
 % Checks whether the group of stones connected to
 % the stone located at (Row, Column) is alive or dead.
 check_alive(Row, Column, Board, Stone, Visited) :-
